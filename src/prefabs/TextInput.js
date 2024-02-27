6c1d4a70e9b395cc5d460d2f693e6cab97d7b860
alhinator <alex@leghart.org>
2024-02-26 23:30:25 -0800

    working on text input

--- src/prefabs/TextInput.js
+++ src/prefabs/TextInput.js
@@ -1,3 +1,53 @@
-class TextInput {
+class TextInput extends Typewriter{
+    constructor(scene, x, y, font, _full_text, _speed = 50, size = 16, align = 'left', _buff_char = '_'){
+        super(scene, x, y, font, _full_text, _speed = 50, size = 16, align = 'left')
+        this.allowInput = true
+        this.hasBufferChar = false
+        this.buffChar = _buff_char
+        this.isBufferOscillating = false
+    }
+    //command funcs
+    lockInput(){
+        this.allowInput = false
+    }
+    allowInput(){
+        this.allowInput = true
+    }
+    startBufferOscillation(){
+        if(this.isBufferOscillating){return}
+        this.bufferOscillation()
+    }
+
+    bufferOscillation(){
+        if(this.hasBufferChar){ //if there is a buffer character at the end, take it off.
+            this.text = this.text.slice(0, -1);
+            this.hasBufferChar = false
+        } else {
+            this.text += this.buffChar
+            this.hasBufferChar = true
+        }
+        this.scene.time.delayedCall(50, ()=>{this.bufferOscillation()}, null, this)
+    }
     
+    //overriding typeGlyph in order to account for endcap character
+    typeGlyph(){
+        //console.log(this.full_text)
+         //add the next index 
+         let charr = this.full_text[this.typeIndex]
+         this.text += charr
+         this.typeIndex++
+         if(isAlphanumeric(charr)){
+             this.scene.sound.play('kpshort',{loop:false, volume:0.2})
+         }
+ 
+         if (this.typeIndex == this.full_text.length){
+             this.state = "done"
+             console.log("done typing")
+             return
+         }
+         let delay = this.goingFast ? this.fastSpeed : this.speed
+         this.scene.time.delayedCall(delay, ()=>{this.typeGlyph()}, null, this)
+ 
+     }
+
 }
--- src/prefabs/Typewriter.js
+++ src/prefabs/Typewriter.js
@@ -2,6 +2,7 @@ class Typewriter extends Phaser.GameObjects.BitmapText{
     constructor(scene, x, y, font, _full_text, _speed = 50, size = 16, align = 'left'){
         super(scene, x, y, font, "", size, align)
         scene.add.existing(this)
+
         this.full_text = _full_text
 
 
@@ -16,6 +17,8 @@ class Typewriter extends Phaser.GameObjects.BitmapText{
         this.isGlowing = false
     }
 
+    //command functions
+
     startTyping(){
         if(this.state != "waiting"){
             return
@@ -24,43 +27,41 @@ class Typewriter extends Phaser.GameObjects.BitmapText{
         this.state = "typing"
         this.typeGlyph()
     }
-
     activateGlow(){
         if(this.isGlowing){return}
         this.isGlowing = true
         this.postFX.addGlow(0x7797e0,1, 0.8, false, 0.1,20)
     }
-
-    setFast(_f){
-        this.goingFast = _f
-    }
-
     typeGlyph(){
-       //console.log(this.full_text)
-        //add the next index 
-        let charr = this.full_text[this.typeIndex]
-        this.text += charr
-        this.typeIndex++
-        if(isAlphanumeric(charr)){
-            this.scene.sound.play('kpshort',{loop:false, volume:0.2})
-        }
+        //console.log(this.full_text)
+         //add the next index 
+         let charr = this.full_text[this.typeIndex]
+         this.text += charr
+         this.typeIndex++
+         if(isAlphanumeric(charr)){
+             this.scene.sound.play('kpshort',{loop:false, volume:0.2})
+         }
+ 
+         if (this.typeIndex == this.full_text.length){
+             this.state = "done"
+             console.log("done typing")
+             return
+         }
+         let delay = this.goingFast ? this.fastSpeed : this.speed
+         this.scene.time.delayedCall(delay, ()=>{this.typeGlyph()}, null, this)
+ 
+     }
 
-        if (this.typeIndex == this.full_text.length){
-            this.state = "done"
-            console.log("done typing")
-            return
-        }
-        let delay = this.goingFast ? this.fastSpeed : this.speed
-        this.scene.time.delayedCall(delay, ()=>{this.typeGlyph()}, null, this)
 
+    //control functions
+    setFast(_b){
+        this.goingFast = _b
     }
-
     append_text_auto_type(the_text) {
         this.full_text += the_text
         //this.state = "waiting"
         this.startTyping()
     }
-
     append_text(the_text){
         this.full_text += the_text
     }
