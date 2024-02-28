3be157ca056a2bbd3238a6b00a859567b2834630
alhinator <alex@leghart.org>
2024-02-27 20:36:46 -0800

    working on city entry

--- src/prefabs/Country.js
+++ src/prefabs/Country.js
@@ -8,11 +8,11 @@ class Country{
 
 //get commands
     getTargets(){
-        let retVal = ""
+        let retVal = "SIGNIFICANT TARGETS IN THE " + this.name + ":\n\n"
         let lr = 0
         for (const key in this.targets) {
             let nm = this.targets[key].name
-            if(lr == 0 || lr == 1){ retVal += nm + "        "}
+            if(lr == 0 || lr == 1){ retVal += nm; for(let i = 0; i < 20-nm.length;i++){retVal+=" "}}
             else { retVal += nm + "\n"}
             lr ++ ; if(lr > 2) {lr = 0}
         }
--- src/prefabs/Target.js
+++ src/prefabs/Target.js
@@ -1,8 +1,9 @@
 class Target {
     constructor(_name, _parent, _population, _hasBunkers){
-        console.log("in city constructor:" + _name)
+        //console.log("in city constructor:" + _name)
         this.name = _name
         this.parent = _parent //parent country
+        this.original_population = _population
         this.population = _population
         this.destroyed = false
         this.isActivelyTargeted = false
